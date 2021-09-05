4bd63f7bdfa9fb8e382b1d20227a3db29ec1e771
Cole Maclean <hi@colemaclean.dev>
2021-09-05 09:44:55 -0700

    feat: passthrough cf request info

--- src/home.ts
+++ src/home.ts
@@ -6,6 +6,18 @@ import indexTemplate from './templates/index.njk';
 export function getHome(request: Request, sentry: Toucan): Promise<Response> {
   const context = {
     currentTime: new Date().toISOString(),
+    asn: request.cf.asn,
+    colo: request.cf.colo,
+    country: request.cf.country,
+    city: request.cf.city,
+    continent: request.cf.continent,
+    latitude: request.cf.latitude,
+    longitude: request.cf.longitude,
+    postalCode: request.cf.postalCode,
+    metroCode: request.cf.metroCode,
+    region: request.cf.region,
+    regionCode: request.cf.regionCode,
+    timezone: request.cf.timezone,
   };
   const headers = {
     Link:
