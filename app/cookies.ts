f0a8876b03671faa15c3d74611aeaec6d301d121
Cole Maclean <hi@colemaclean.dev>
2022-02-09 21:39:52 -0800

    fix: cookie settings

--- app/cookies.ts
+++ app/cookies.ts
@@ -2,4 +2,7 @@ import { createCookie } from 'remix';
 
 export const themeCookie = createCookie('theme', {
   maxAge: 31536000, // one year
+  sameSite: 'strict',
+  secure: true,
+  httpOnly: false,
 });
