93077a06850137efc0b133460f3c3d6b9fc4ff51
Cole Maclean <hi@colemaclean.dev>
2022-04-26 20:01:45 +0000

    feat: add image to about page

--- app/routes/about.tsx
+++ app/routes/about.tsx
@@ -1,17 +1,28 @@
 export default function About() {
   return (
-    <div>
+    <div className="about">
       <h2 id="about-me">About Me</h2>
-      <p>I'm a software developer living and working in Vancouver, Canada.</p>
-      <p>
-        You can find some of my work on{' '}
-        <a href="https://github.com/cole/" rel="me">
-          GitHub
-        </a>
-        . My most popular project is{' '}
-        <a href="https://pypi.org/project/aiosmtplib/">aiosmtplib</a>, a library
-        for sending email asynchronously in Python.
-      </p>
+      <div className="split">
+        <img
+          src="/images/portrait.jpg"
+          srcSet="/images/portrait.jpg 1x, /images/portrait-2x.jpg 2x"
+          alt="Portrait of the author, Cole Maclean"
+        />
+        <div>
+          <p>
+            I'm a software developer living and working in Vancouver, Canada.
+          </p>
+          <p>
+            You can find some of my work on{' '}
+            <a href="https://github.com/cole/" rel="me">
+              GitHub
+            </a>
+            . My most popular project is{' '}
+            <a href="https://pypi.org/project/aiosmtplib/">aiosmtplib</a>, a
+            library for sending email asynchronously in Python.
+          </p>
+        </div>
+      </div>
       <h2 id="about-this-site">About This Site</h2>
       <p>
         This site is built with <a href="https://remix.run">Remix</a>. It runs
