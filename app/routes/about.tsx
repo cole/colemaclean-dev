d50e0b4db461953ab0fd90cb93f4bd5527a0419b
Cole Maclean <hi@colemaclean.dev>
2022-04-26 18:08:59 +0000

    bugfix: title case

--- app/routes/about.tsx
+++ app/routes/about.tsx
@@ -1,7 +1,7 @@
 export default function About() {
   return (
     <div>
-      <h2 id="about-me">About me</h2>
+      <h2 id="about-me">About Me</h2>
       <p>I'm a software developer living and working in Vancouver, Canada.</p>
       <p>
         You can find some of my work on{' '}
@@ -12,7 +12,7 @@ export default function About() {
         <a href="https://pypi.org/project/aiosmtplib/">aiosmtplib</a>, a library
         for sending email asynchronously in Python.
       </p>
-      <h2 id="about-this-site">About this site</h2>
+      <h2 id="about-this-site">About This Site</h2>
       <p>
         This site is built with <a href="https://remix.run">Remix</a>. It runs
         on <a href="https://workers.cloudflare.com/">Cloudflare Workers</a>.
