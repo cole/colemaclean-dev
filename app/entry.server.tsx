1e2093cae62b01e2f7c53b1f037827892ab12252
Cole Maclean <hi@colemaclean.dev>
2022-04-26 21:01:03 -0700

    Revert "feat: switch to readable streams"
    This reverts commit 1fe7d1f697d11ca58bb5f7b99209e362f0b3e987.

--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -1,41 +1,21 @@
-import { renderToReadableStream } from 'react-dom/server';
+import { renderToString } from 'react-dom/server';
 import { RemixServer } from 'remix';
 import type { EntryContext } from 'remix';
 
-export default async function handleRequest(
+export default function handleRequest(
   request: Request,
   responseStatusCode: number,
   responseHeaders: Headers,
   remixContext: EntryContext,
 ) {
-  const controller = new AbortController();
-  let didError = false;
+  const markup = renderToString(
+    <RemixServer context={remixContext} url={request.url} />,
+  );
 
-  try {
-    const stream = await renderToReadableStream(
-      <RemixServer context={remixContext} url={request.url} />,
-      {
-        signal: controller.signal,
-        onError(error) {
-          didError = true;
-          console.error(error);
-        },
-      },
-    );
+  responseHeaders.set('Content-Type', 'text/html');
 
-    responseHeaders.set('Content-Type', 'text/html');
-
-    return new Response(stream, {
-      status: didError ? 500 : responseStatusCode,
-      headers: responseHeaders,
-    });
-  } catch (error) {
-    return new Response(
-      `<!doctype html><html><body><p>${error.toString()}</p></body></html>`,
-      {
-        status: 500,
-        headers: { 'Content-Type': 'text/html' },
-      },
-    );
-  }
+  return new Response('<!DOCTYPE html>' + markup, {
+    status: responseStatusCode,
+    headers: responseHeaders,
+  });
 }
--- worker/index.js
+++ worker/index.js
@@ -1,36 +1,5 @@
-import {
-  createRequestHandler,
-  handleAsset,
-} from "@remix-run/cloudflare-workers";
+import { createEventHandler } from "@remix-run/cloudflare-workers";
 
 import * as build from "../build";
 
-const handleRequest = createRequestHandler({ build });
-
-const handleEvent = async (event) => {
-  let response = await handleAsset(event, build);
-
-  if (!response) {
-	response = await handleRequest(event);
-  }
-
-  return response;
-};
-
-addEventListener("fetch", (event) => {
-  try {
-	event.respondWith(handleEvent(event));
-  } catch (e) {
-	if (process.env.NODE_ENV === "development") {
-	  event.respondWith(
-		new Response(e.message || e.toString(), {
-		  status: 500,
-		})
-	  );
-	}
-
-	event.respondWith(
-	  new Response("Internal Error", { status: 500 })
-	);
-  }
-});
+addEventListener("fetch", createEventHandler({ build }));
