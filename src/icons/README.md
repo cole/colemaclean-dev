4664623abe9082f6c53ae63b9b12dafa0050a509
Cole Maclean <hi@colemaclean.dev>
2022-09-04 13:19:25 -0700

    feat: local svg components

---
+++ src/icons/README.md
@@ -0,0 +1,3 @@
+# Mutant.tech icons converted to Astro components
+
+Process is still manual; run svgo on the component, then use one of the existing components as a template.
---
+++ src/icons/index.ts
@@ -0,0 +1,6 @@
+import CrtPrompt from './CrtPrompt.astro';
+import CrtBlueScreen from './CrtBlueScreen.astro';
+import LightBulb from './LightBulb.astro';
+import WaveHmn5 from './WaveHmn5.astro';
+
+export { CrtBlueScreen, CrtPrompt, LightBulb, WaveHmn5 };
