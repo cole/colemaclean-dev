4dc9cd81507aa15f7c13aaf08759f7737abd3b4c
Cole Maclean <hi@colemaclean.dev>
2022-09-14 21:19:21 -0700

    add netlify config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,8 @@
 import { defineConfig } from 'astro/config';
-import node from '@astrojs/node';
+import netlify from '@astrojs/netlify/edge-functions';
 
 // https://astro.build/config
 export default defineConfig({
-    output: 'server',
-    adapter: node(),
+  output: 'server',
+  adapter: netlify()
 });
