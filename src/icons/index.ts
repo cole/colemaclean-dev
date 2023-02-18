a8d37b239275e10354e9ebf4b2906b71b73ffffb
Cole Maclean <hi@colemaclean.dev>
2023-02-17 21:38:50 -0800

    chore: ts cleanup

--- src/icons/index.ts
+++
@@ -1,6 +0,0 @@
-import CrtPrompt from './CrtPrompt.astro';
-import CrtBlueScreen from './CrtBlueScreen.astro';
-import LightBulb from './LightBulb.astro';
-import WaveHmn5 from './WaveHmn5.astro';
-
-export { CrtBlueScreen, CrtPrompt, LightBulb, WaveHmn5 };
